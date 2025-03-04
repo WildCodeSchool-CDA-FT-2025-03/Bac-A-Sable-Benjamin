7416805ff2a00e020e7ca202dda3ef15cd573f9a
OOOSNA <Dilgopierre@gmail.com>
2025-03-04 14:50:37 +0100

    :hammer: Add auto load router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import { loadRoutes } from './utils/loadRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
   res.send('Ok ! 🧙‍♂️');
 });
+
+loadRoutes(app, "src/routes");
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port http://localhost:${PORT} 🦥`);
