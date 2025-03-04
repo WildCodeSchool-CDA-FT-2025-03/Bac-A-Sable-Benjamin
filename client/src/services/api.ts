dd7bf75408f78d50796f8a4877b981287feed49a
OOOSNA <Dilgopierre@gmail.com>
2025-03-04 17:06:40 +0100

    :hammer: Change api url to env

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3005/api',
+  baseURL: apiUrl + "/api",
 });
