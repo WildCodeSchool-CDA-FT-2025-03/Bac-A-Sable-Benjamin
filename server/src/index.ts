4d09c0a3df992143fcbc7997a341e0917fa5004f
OOOSNA <Dilgopierre@gmail.com>
2025-03-04 16:12:10 +0100

    :package: :hammer: (server) Add cors and dotenv package

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,20 @@
 import express, { Request, Response } from 'express';
 import { loadRoutes } from './utils/loadRoutes';
+import { config } from 'dotenv';
+import cors from 'cors';
+
+config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Ok ! 🧙‍♂️');
