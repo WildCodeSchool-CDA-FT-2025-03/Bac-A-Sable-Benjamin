d7d4481cdedf9add596110193515534df30b5745
OOOSNA <Dilgopierre@gmail.com>
2025-03-04 16:19:17 +0100

    :hammer: (client) Add layout with Footer component

---
+++ client/src/components/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+export default function Layout() {
+    return (
+        <>
+            <Outlet />
+            <Footer />
+        </>
+    );
+}
+
+
